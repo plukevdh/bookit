c7523726f625f8c14873e10fcd916a5f76c1b65c
plukevdh <hungerandthirst@gmail.com>
2011-05-11 01:42:47 -0400

    more examples

--- examples/test.rb
+++ examples/test.rb
@@ -1,8 +1,8 @@
-$: << './lib/'
+$: << '../lib/'
 require 'bookit'
 
 base = {
-  content: File.read('./examples/article3.html'),
+  content: File.read('article2.html'),
   author: "What?",
   title: "Who?",
   date_published: Time.now.to_s,
@@ -11,4 +11,6 @@ base = {
 
 a = Bookit::Article.new(base, Bookit::Parser::Html)
 
-a.to_print(Bookit::Emitter::Epub)
+content = a.to_print(Bookit::Emitter::Mobi).render
+puts content
+
